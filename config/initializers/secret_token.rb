be473a958bfa5731fd242d3399705b56bc56c95e
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2012-01-29 15:38:23 +0900

    rails new nico_cal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NicoCal::Application.config.secret_token = '01244e1cc14b85ed52863b1fa30a16dd924cdb81775b716349495202bc13b584dfefc861ae47a32571b1bae9cf9062265d558b9416ef4dc6b4bb679edd608ff3'
